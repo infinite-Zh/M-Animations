2097cb367e53fbdc1592701c2823b0fbd44ada84
infinite <infinite.xu@gmail.com>
2016-11-01 17:31:34 +0800

    progressing

--- app/src/main/java/com/infinite/m_animations/MainActivity.java
+++ app/src/main/java/com/infinite/m_animations/MainActivity.java
@@ -1,10 +1,15 @@
 package com.infinite.m_animations;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.recycler)
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
